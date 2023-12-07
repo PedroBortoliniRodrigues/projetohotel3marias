a8a9d1e00a518adc8a94ecc5ae2652d64956fba2
Pedro Henrique Bortolini Rodrigues <89049144+PedroBortoliniRodrigues@users.noreply.github.com>
2023-12-07 13:00:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal(`.bemvindo`, {duration: 3000});
+
+sr.reveal(`.corpo`, {duration: 3000});
+
+sr.reveal(`.corpo1`, {duration: 3000});
+
+sr.reveal(`.mapa`, {duration: 3000});
+
+sr.reveal(`.contato`, {duration: 3000});
+
